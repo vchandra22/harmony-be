bc314fef205833b7d8509dbe5c628b5cc695f414
vchandra22 <vincentchaandra@gmail.com>
2025-02-26 07:33:04 +0700

    feat: initialize springboot application

---
+++ src/main/java/com/hris/harmony/HarmonyApplication.java
@@ -0,0 +1,14 @@
+package com.hris.harmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonyApplication.class, args);
+        System.out.println("Hello World!!!");
+    }
+
+}
